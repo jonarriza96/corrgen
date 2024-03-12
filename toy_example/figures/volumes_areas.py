ce4f9cbd7ecea39c525a3e29f517f3e70b0176bc
Jon Arrizabalaga <arrijon96@gmail.com>
2024-03-12 16:55:00 -0400

    finished with second iteration of plots

--- toy_example/figures/volumes_areas.py
+++ toy_example/figures/volumes_areas.py
@@ -4,26 +4,27 @@ import matplotlib.pyplot as plt
 import pickle
 
 
-save_path = "/home/jonarriza96/corrgen/paper/figures/"
+save_path = "/home/jonarriza96/corrgen_v2/toy_example/figures/figures/"
 
 
 print("Importing data...")
 areas_path = "/home/jonarriza96/corrgen_v2/toy_example/figures/data/2d/areas.pkl"
-volumes_path = "/home/jonarriza96/corrgen/examples/experiments/data/3d/"
+volumes_path = "/home/jonarriza96/corrgen_v2/toy_example/figures/data/3d/"
 with open(areas_path, "rb") as f:
     data = pickle.load(f)
     area_degs = data["poly_degs"]
     areas = data["areas"]
 
-volume_degs = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+volume_degs = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
 volumes = []
 for i in volume_degs:
     with open(volumes_path + str(i) + "_SDP.pkl", "rb") as f:
         data = pickle.load(f)
-        volumes += data["corridor_volume"]
+        volumes += [data["corridor_volume"]]
 print("Done.")
 
 
+# %%
 colors = plt.rcParams["axes.prop_cycle"].by_key()["color"]
 fs = 14
 fig, ax = plt.subplots(nrows=2, sharex=True)
@@ -36,13 +37,28 @@ ax[0].set_yticks([1, 3, 5], fontsize=fs)
 ax[0].grid(axis="x")
 
 ax[0].plot(
-    [3, 3], [0.6 * min(areas), 1.1 * max(areas)], "-", color=colors[0], alpha=0.5
+    [3, 3],
+    [0.6 * min(areas), 1.1 * max(areas)],
+    "-",
+    color=colors[0],
+    alpha=0.5,
+    linewidth=2,
 )
 ax[0].plot(
-    [6, 6], [0.6 * min(areas), 1.1 * max(areas)], "-", color=colors[1], alpha=0.5
+    [6, 6],
+    [0.6 * min(areas), 1.1 * max(areas)],
+    "-",
+    color=colors[1],
+    alpha=0.5,
+    linewidth=2,
 )
 ax[0].plot(
-    [15, 15], [0.6 * min(areas), 1.1 * max(areas)], "-", color=colors[2], alpha=0.5
+    [15, 15],
+    [0.6 * min(areas), 1.1 * max(areas)],
+    "-",
+    color=colors[2],
+    alpha=0.5,
+    linewidth=2,
 )
 ax[0].set_ylim([0.6 * min(areas), 1.1 * max(areas)])
 
@@ -52,10 +68,20 @@ ax[1].plot(6, volumes[3], marker="o", color=colors[1], alpha=0.95, markersize=8)
 ax[1].plot(15, volumes[12], marker="o", color=colors[2], alpha=0.95, markersize=8)
 
 ax[1].plot(
-    [3, 3], [0.975 * min(volumes), 1.02 * max(volumes)], "-", color=colors[0], alpha=0.5
+    [3, 3],
+    [0.975 * min(volumes), 1.02 * max(volumes)],
+    "-",
+    color=colors[0],
+    alpha=0.5,
+    linewidth=2,
 )
 ax[1].plot(
-    [6, 6], [0.975 * min(volumes), 1.02 * max(volumes)], "-", color=colors[1], alpha=0.5
+    [6, 6],
+    [0.975 * min(volumes), 1.02 * max(volumes)],
+    "-",
+    color=colors[1],
+    alpha=0.5,
+    linewidth=2,
 )
 ax[1].plot(
     [15, 15],
@@ -63,14 +89,15 @@ ax[1].plot(
     "-",
     color=colors[2],
     alpha=0.5,
+    linewidth=2,
 )
-ax[1].set_ylim([0.975 * min(volumes), 1.02 * max(volumes)])
+ax[1].set_ylim([0.94 * min(volumes), 1.02 * max(volumes)])
 ax[1].set_ylabel("Volume", fontsize=fs)
 
 ax[1].set_xlabel("Polynomial degree", fontsize=fs)
 # ax[1].set_xticks([2,8,14,20])
 ax[1].set_xticks([3, 6, 9, 12, 15, 18])
-ax[1].set_yticks([6, 7, 8])
+# ax[1].set_yticks([6, 7, 8])
 ax[1].grid(axis="x")
 
 fig.set_size_inches(4.3, 3.5)
